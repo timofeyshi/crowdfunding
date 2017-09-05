3585dbd177bc5458a4d3a5c441a291e6fac00271
timofeyshiPr <timashishov2@gmail.com>
2017-09-05 13:16:00 +0300

    Add CRUD

---
+++ public/controllers/controller.js
@@ -0,0 +1,12 @@
+var app = angular.module('myApp', []);
+
+app.controller('myCtrl', function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+
+    project = {
+    	name: "first_robot",
+    	description: "temp@temp.tie;mkfdl dkfm"
+    }
+
+});
