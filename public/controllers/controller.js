dc4bdca5a1dc481c9bcb85f9a72ea059ce6124cc
timofeyshiPr <timashishov2@gmail.com>
2017-09-12 09:57:15 +0300

    Add routing (angular)

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,12 +1,32 @@
-var app = angular.module('myApp', []);
+angular.module('RoutingApp', ['ngRoute'])
+  .config( ['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/index.html'
+  })
+  .when('/signin', {
+    templateUrl: 'views/signin.html'
+  })
+.when('/register', {
+    templateUrl: 'views/register.html'
+  })
+.when('/panel', {
+    templateUrl: 'views/settings.html'
+  })
+.when('/projects', {
+    templateUrl: 'views/panel.html'
+  })
+.when('/add', {
+    templateUrl: 'views/add.html'
+  })
+.when('/all', {
+    templateUrl: 'views/projects.html'
+  })
+.when('/project', {
+    templateUrl: 'views/index2.html'
+  })
 
-app.controller('myCtrl', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-
-    project = {
-    	name: "first_robot",
-    	description: "temp@temp.tie;mkfdl dkfm"
-    }
-
-});
+  .otherwise({
+    redirectTo: '/'
+  });
+}]);
